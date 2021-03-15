94d4f1b8350d8fc5e3de265bc67811011d493b9d
Rositsa Ruseva <71460194+RositsaRuseva@users.noreply.github.com>
2021-03-15 23:00:12 +0200

    Create StartUp.cs

---
+++ Simple Snake/StartUp.cs
@@ -0,0 +1,22 @@
+namespace SimpleSnake
+{
+    using SimpleSnake.Core;
+    using SimpleSnake.GameObjects;
+    using System.Collections.Generic;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ConsoleWindow.CustomizeConsole();
+
+
+            Wall wall = new Wall(60, 20);
+            Snake snake = new Snake(wall);
+
+            Engine engine = new Engine(snake, wall);
+            engine.Run();
+        }
+    }
+}
